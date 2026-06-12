4f3c48cc6ce482221bb52b1acf89274045659ab7
Ritesh Kumar Singh <reenasingh68204@gmail.com>
2026-06-13 03:04:59 +0530

    typecasting

--- pythonday9.py
+++ pythonday9.py
@@ -1,2 +1,12 @@
 # typecasting
 
+a= 10
+b= 20.5    
+c= "Hello"
+print(type(a))
+print(type(b))  
+print(type(c))
+print(int(a)+int(b))  # explicit typecasting b to int before addition
+print(float(a)+float(b))  # explicit typecasting a to float before addition
+print(a+b)  # no typecasting, will perform addition as is and implicitly convert a to float before addition
+print(str(a)+str(b))  # explicit typecasting both a and b to string before concatenation
